1dee0fa879bea27cfa0626359658e709d1ffacce
Mahmood <M26967@uk.emea.ngint.com>
2019-06-18 14:08:34 +0100

    added registration page

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -3,16 +3,150 @@ import withStyles from "@material-ui/core/styles/withStyles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { Link, withRouter } from "react-router-dom";
 import myStyles from "./styles";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import { connect } from "react-redux";
 
 const styles = myStyles;
 class Registration extends Component {
+  state = {
+    firstName: "",
+    lastName: "",
+    userName: "",
+    emailAddress: "",
+    password: "",
+    confPassword: "",
+    firstNameHelperText: "",
+    lastNameHelperText: "",
+    userNameHelperText: "",
+    emailAddressHelperText: "",
+    passwordHelperText: "",
+    confPasswordHelperText: "",
+    errorFirstName: false,
+    errorLastName: false,
+    errorUserName: false,
+    errorEmailAddress: false,
+    errorPassword: false,
+    errorConfPassword: false,
+    disabled: true
+  };
+
+  onChangeFirstName = fname => {
+    this.props.onEnterFirstName(fname);
+    if (fname.target.value.length > 0) {
+      this.setState({
+        firstName: fname.target.value,
+        errorFirstName: false,
+        firstNameHelperText: ""
+      });
+    } else {
+      this.setState({
+        firstName: fname.target.value,
+        errorFirstName: true,
+        firstNameHelperText: "Please enter a first name"
+      });
+      console.log("Please enter a first name");
+    }
+  };
+
+  onChangeLastName = lname => {
+    this.props.onEnterLastName(lname);
+    if (lname.target.value.length > 0) {
+      this.setState({
+        lastName: lname.target.value,
+        errorLastName: false,
+        lastNameHelperText: ""
+      });
+    } else {
+      this.setState({
+        lastName: lname.target.value,
+        errorLastName: true,
+        lastNameHelperText: "Please enter a last name"
+      });
+      console.log("Please enter a last name");
+    }
+  };
+
+  onChangeUserName = user => {
+    this.props.onEnterUserName(user);
+    if (user.target.value.length > 0) {
+      this.setState({
+        userName: user.target.value,
+        errorUserName: false,
+        userNameHelperText: ""
+      });
+    } else {
+      this.setState({
+        userName: user.target.value,
+        errorUserName: true,
+        userNameHelperText: "Please enter a user name"
+      });
+      console.log("Please enter a user name");
+    }
+  };
+
+  onChangeEmailAddress = email => {
+    this.props.onEnterEmailAddress(email);
+    if (email.target.value.length > 0) {
+      this.setState({
+        emailAddress: email.target.value,
+        errorEmailAddress: false,
+        emailAddressHelperText: ""
+      });
+    } else {
+      this.setState({
+        emailAddress: email.target.value,
+        errorEmailAddress: true,
+        emailAddressHelperText: "Please enter a email address"
+      });
+      console.log("Please enter a email address");
+    }
+  };
+
+  onChangePassword = password => {
+    this.props.onEnterPassword(password);
+    if (password.target.value.length > 0) {
+      this.setState({
+        password: password.target.value,
+        errorPassword: false,
+        passwordHelperText: ""
+      });
+    } else {
+      this.setState({
+        password: password.target.value,
+        errorPassword: false,
+        passwordHelperText: "Please enter a password"
+      });
+      console.log("Please enter a password");
+    }
+  };
+
+  onChangeConfPassword = cpassword => {
+    this.props.onEnterConfPassword(cpassword);
+    if (cpassword.target.value.length > 0) {
+      this.setState({
+        confPassword: cpassword.target.value,
+        errorConfPassword: false,
+        confPasswordHelperText: ""
+      });
+    } else {
+      this.setState({
+        confPassword: cpassword.target.value,
+        errorConfPassword: true,
+        confPasswordHelperText: "Please enter a confirmation password"
+      });
+      console.log("Please enter a conf password");
+    }
+  };
+
   render() {
+    const { classes } = this.props;
     return (
-      <div>
+      <div className={classes.root}>
         <CssBaseline />
-<<<<<<< Updated upstream
-        Registration Form changeMe!!!!
-=======
         <Grid container spacing={12} className={classes.grid} justify="center">
           <Typography color="secondary" variant="h2">
             Registration form
@@ -94,7 +228,7 @@ class Registration extends Component {
                   variant="contained"
                   color="secondary"
                   component={Link}
-                  to={{ pathname: "/registrationsuccess" }}
+                  to={{ pathname: "/home" }}
                 >
                   Enter
                 </Button>
@@ -102,7 +236,6 @@ class Registration extends Component {
             </Grid>
           </Paper>
         </Grid>
->>>>>>> Stashed changes
       </div>
     );
   }
